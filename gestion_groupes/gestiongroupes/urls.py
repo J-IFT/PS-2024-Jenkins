a4947390ad4994ef48abdd8c206934743d47ccf2
LorisPercin <74607168+LorisPercin@users.noreply.github.com>
2024-03-28 10:49:09 +0100

    ✨ add basic pages templates

--- gestion_groupes/gestiongroupes/urls.py
+++ gestion_groupes/gestiongroupes/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('liste/', views.users_list, name='users_list'),
+    path('config/', views.group_config, name='group_config'),
+    path('details/<int:group_id>/', views.group_details, name='group_details'),
 ]
--- gestion_groupes/gestiongroupes/views.py
+++ gestion_groupes/gestiongroupes/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'gestiongroupes/index.html')
+
+
+def users_list(request):
+    context = {'users': ['Alice', 'Bob', 'Charlie']}
+    return render(request, 'gestiongroupes/users_list.html', context)
+
+
+def group_config(request):
+    return render(request, 'gestiongroupes/group_config.html')
+
+
+def group_details(request, group_id):
+    context = {'name': "Groupe " + str(group_id), 'members': ['Dylan', 'Eddy']}
+    return render(request, 'gestiongroupes/group_details.html', context)
