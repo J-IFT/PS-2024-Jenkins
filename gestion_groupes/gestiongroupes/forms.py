a24f701e1e92d6782aaf089dbd26ea9de724d7a3
LorisPercin <74607168+LorisPercin@users.noreply.github.com>
2024-04-03 10:15:56 +0200

    ♻️ change last_group field to radio instead of select

--- gestion_groupes/gestiongroupes/forms.py
+++ gestion_groupes/gestiongroupes/forms.py
@@ -1,17 +1,22 @@
 from django import forms
+from django.forms import ModelForm
+
+from gestiongroupes.models import GroupConfig
 
 
 class ConnectionForm(forms.Form):
     username = forms.CharField(label="Nom d'utilisateur :", max_length=25)
 
 
-class GroupConfigForm(forms.Form):
+class GroupConfigForm(ModelForm):
+    # Obligé de redéfinir le champ pour l'afficher en radio plutôt que select
     LAST_GROUP_CHOICES = [
         ('LAST_MAX', 'Plus grand'),
         ('LAST_MIN', 'Plus petit')
     ]
 
-    max_users = forms.IntegerField(label="Nombre max. d'utilisateurs")
-    max_groups = forms.IntegerField(label="Nombre max. de groupes")
-    last_group = forms.ChoiceField(label="Configuration du dernier groupe", choices=LAST_GROUP_CHOICES,
-                                   widget=forms.RadioSelect())
+    last_group = forms.ChoiceField(choices=LAST_GROUP_CHOICES, widget=forms.RadioSelect)
+
+    class Meta:
+        model = GroupConfig
+        fields = ['max_users', 'max_groups', 'last_group']
--- gestion_groupes/gestiongroupes/models.py
+++ gestion_groupes/gestiongroupes/models.py
@@ -1,3 +1,14 @@
+from django import forms
 from django.db import models
 
+
 # Create your models here.
+class GroupConfig(models.Model):
+    LAST_GROUP_CHOICES = [
+        ('LAST_MAX', 'Plus grand'),
+        ('LAST_MIN', 'Plus petit')
+    ]
+
+    max_users = models.IntegerField()
+    max_groups = models.IntegerField()
+    last_group = models.CharField(max_length=8, choices=LAST_GROUP_CHOICES)
