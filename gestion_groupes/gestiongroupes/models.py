3e07895e69d1f4a5f307962c466bb65120f49467
LorisPercin <loris.percin@outlook.fr>
2024-04-15 17:02:49 +0200

    :recycle: move get_group_sizes to GroupConfig class

--- gestion_groupes/gestiongroupes/models.py
+++ gestion_groupes/gestiongroupes/models.py
@@ -13,3 +13,14 @@ class GroupConfig(models.Model):
     last_group = models.CharField(max_length=8, choices=LAST_GROUP_CHOICES, default=LAST_GROUP_CHOICES[0][0])
     group_size = models.IntegerField(default=5)
     last_group_size = models.IntegerField(default=5)
+
+    @staticmethod
+    def get_group_sizes(max_users, max_groups, last_group):
+        base_size = max_users // max_groups
+        remainder = max_users % max_groups
+        last_group_size = base_size + remainder
+
+        if remainder != 0 and last_group == 'LAST_MIN':
+            return base_size + 1, last_group_size - (max_groups - 1)
+
+        return base_size, last_group_size
