d0a5573b6fe58ccea966c4b1d8d2696a6e8cf657
LorisPercin <loris.percin@outlook.fr>
2024-04-25 11:48:52 +0200

    🚧 get_nb_group_with_max_members unit tests

--- gestion_groupes/gestiongroupes/tests.py
+++ gestion_groupes/gestiongroupes/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from gestiongroupes.models import GroupConfig
+
+from .models import Groupe, GroupConfig, Utilisateur
+from .tools import init, get_group_config, get_nb_group_with_max_members
 
 
 class GroupConfigTestCase(TestCase):
@@ -18,3 +20,45 @@ class GroupConfigTestCase(TestCase):
         self.assertEqual(GroupConfig.get_group_sizes(19, 5, 'LAST_MIN'), (4, 3))
         self.assertEqual(GroupConfig.get_group_sizes(20, 5, 'LAST_MIN'), (4, 4))
         self.assertEqual(GroupConfig.get_group_sizes(21, 5, 'LAST_MIN'), (5, 1))
+
+
+class ToolsTestCase(TestCase):
+    def setUp(self):
+        GroupConfig.objects.create(max_users=19, max_groups=5, last_group='LAST_MAX', group_size=3, last_group_size=7)
+
+    def test_no_group_created(self):
+        self.assertEqual(get_nb_group_with_max_members(), False)
+
+    def test_no_maxed_out_group(self):
+        self.group = Groupe.objects.create()
+        self.group.utilisateurs.add(Utilisateur.objects.create(nom="Alice"))
+        self.group.utilisateurs.add(Utilisateur.objects.create(nom="Bob"))
+        self.group2 = Groupe.objects.create()
+        self.group2.utilisateurs.add(Utilisateur.objects.create(nom="Charlie"))
+        self.group2.utilisateurs.add(Utilisateur.objects.create(nom="Dave"))
+
+        self.assertEqual(get_nb_group_with_max_members(), False)
+
+    def test_maxed_out_small_group(self):
+        self.group = Groupe.objects.create()
+        self.group.utilisateurs.add(Utilisateur.objects.create(nom="Alice"))
+        self.group.utilisateurs.add(Utilisateur.objects.create(nom="Bob"))
+        self.group.utilisateurs.add(Utilisateur.objects.create(nom="Charlie"))
+
+        self.assertEqual(get_nb_group_with_max_members(), 1)
+
+        self.group2 = Groupe.objects.create()
+        self.group2.utilisateurs.add(Utilisateur.objects.create(nom="Dave"))
+        self.group2.utilisateurs.add(Utilisateur.objects.create(nom="Erin"))
+        self.group2.utilisateurs.add(Utilisateur.objects.create(nom="Frank"))
+
+        self.assertEqual(get_nb_group_with_max_members(), 2)
+
+    def test_over_small_group_max(self):
+        self.group = Groupe.objects.create()
+        self.group.utilisateurs.add(Utilisateur.objects.create(nom="Alice"))
+        self.group.utilisateurs.add(Utilisateur.objects.create(nom="Bob"))
+        self.group.utilisateurs.add(Utilisateur.objects.create(nom="Charlie"))
+        self.group.utilisateurs.add(Utilisateur.objects.create(nom="Dave"))
+
+        self.assertEqual(get_nb_group_with_max_members(), False)
